662435a788ba7633a410e6e882195f01ffb26237
Connor <Woodconnor13@gmail.com>
2022-07-05 16:41:58 -0400

    Finished circles lab

---
+++ CirclesLab/Circle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CirclesLab
+{
+    internal class Circle
+    {
+        public double radius;
+
+        //constructor
+        public Circle(double _radius)
+        {
+            radius = _radius;
+        }
+
+        //methods
+        public double CalculateCircumference()
+        {
+            double r = radius * 2 * Math.PI;
+            return r;
+        }
+        public double CalculateArea()
+        {
+            double a = Math.PI * radius * radius;
+            return a;
+        }
+        private string FormatNumber(double x)
+        {
+            string format = string.Format("{0:0.00}", x);
+            return format;
+        }
+        public string CalculateFormattedCircumference()
+        {
+            string formatCircum = FormatNumber(CalculateCircumference());
+            return formatCircum;
+        }
+        public string CalculateFormattedArea()
+        {
+            string area = FormatNumber(CalculateArea());
+            return area;
+        }
+    }
+}
---
+++ CirclesLab/Program.cs
@@ -0,0 +1,66 @@
+﻿using CirclesLab;
+
+Console.WriteLine("Welcome to the Circle Tester.");
+double radius;
+int count = 0;
+bool runProgram = true;
+while (runProgram)
+{
+    while (true)
+    {
+        Console.WriteLine("Please enter a radius: ");
+        //validating user entry
+        if (double.TryParse(Console.ReadLine(), out radius) && radius > 0)
+        {
+            //if number is valid double and over 0 program accepts
+            Console.WriteLine("Calculating...");
+            break;
+        }
+        else
+        {
+            //if number can't parse or less than 0 - rejects
+            Console.WriteLine("Please enter a valid radius!");
+        }
+    }
+
+    //creating an instance of circle
+    Circle myCircle = new Circle(radius);
+    Console.Write("Circumference: ");
+    Console.WriteLine(myCircle.CalculateFormattedCircumference());
+    Console.Write("Area: ");
+    Console.WriteLine(myCircle.CalculateFormattedArea());
+    Console.WriteLine("");
+    runProgram = GoAgain();
+    count++;
+    
+}
+Console.WriteLine($"Thanks for using our program!  You built {count} circles.");
+
+//Josh's run again method
+static bool GoAgain()
+{
+    while (true)
+    {
+        Console.WriteLine("Do you want to go again(y/n)?");
+        string input = Console.ReadLine();
+        try
+        {
+            if (input.ToLower().Trim() == "y")
+            {
+                return true;
+            }
+            else if (input.ToLower().Trim() == "n")
+            {
+                return false;
+            }
+            else
+            {
+                throw new Exception("Not a valid option, please try again.");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
